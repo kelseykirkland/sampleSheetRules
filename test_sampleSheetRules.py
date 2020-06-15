f0d7659b27501e8ca3261700d6ae7c32de852483
kelseykirkland <kelseykirklandwork@gmail.com>
2020-06-15 02:11:08 -0400

    test case disallowed chars

--- test_sampleSheetRules.py
+++ test_sampleSheetRules.py
@@ -1,9 +1,13 @@
 import pytest
-#from sampleSheetRules import checkforspaces
-from sampleSheetRules import checkforspaces
+
+# from sampleSheetRules import checkforspaces
+from sampleSheetRules import checkforspaces, disallowedchars
 
 
 def test_checkforspaces():
     assert checkforspaces("Hello Me")
 
 
+def test_disallowedchars():
+    discharlist = ["#", "*", ".", "\\", "/", "[", "]", ":", ";", "|", "="]
+    assert disallowedchars("hello#me", discharlist)
